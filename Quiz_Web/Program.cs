c0489f98acf5488536f4a330589d04f12e5eb55d
Drista4real <beatwinner123@gmail.com>
2026-06-19 14:00:34 +0700

    fix: register password reset email service

--- Quiz_Web/Program.cs
+++ Quiz_Web/Program.cs
@@ -50,7 +50,7 @@ builder.Services.AddDbContext<LearningPlatformContext>(options =>
 builder.Services.AddScoped<IUserService, UserService>();
 // builder.Services.AddScoped<ILibraryService, LibraryService>();
 builder.Services.AddScoped<ITestService, TestService>();
-// builder.Services.AddScoped<IEmailService, EmailService>();
+builder.Services.AddScoped<IEmailService, EmailService>();
 builder.Services.AddScoped<IFlashcardService, FlashcardService>();
 // builder.Services.AddScoped<ICourseService, CourseService>();
 // builder.Services.AddScoped<IReviewService, ReviewService>();
